f910c4b2d47e674c0ae49f02c48d512e30bdcf56
dacfnetic <dacf9x@gmail.com>
2022-07-29 21:37:23 -0600

    Edit control.js and Edit estilo.css and Edit index.html

--- control.js
+++ control.js
@@ -1,6 +1,21 @@
 const imagen = document.getElementById("uno");
+const clicker = document.getElementById("dos");
 let esP = true;
+let cerrada = true;
+let popup = document.getElementById("popup");
+
+function openPopup(){
+    popup.classList.add("open-popup");
+}
+function closePopup(){
+    popup.classList.remove("open-popup");
+    cerrada = true;
+}
+
+
+
 imagen.addEventListener("click",expandir);
+clicker.addEventListener("click",abrir);
 function expandir(){
     if(esP){
         imagen.style.width = "50%";
@@ -9,4 +24,13 @@ function expandir(){
     }
     imagen.style.width = "100%";
     esP = true;
+}
+function abrir(){
+    if(cerrada){
+        openPopup();
+        cerrada = false;
+        return false;
+    }
+    closePopup();
+    cerrada = true;
 }
